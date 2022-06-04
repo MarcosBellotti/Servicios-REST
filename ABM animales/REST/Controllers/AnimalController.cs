dd44c68fcdbbb4cf385012830c03ad3bcaea5f17
MarcosBellotti5 <marcosbellotti5@gmail.com>
2022-06-04 10:43:59 -0300

    Ver como hacer las validaciones por error

--- ABM animales/REST/Controllers/AnimalController.cs
+++ ABM animales/REST/Controllers/AnimalController.cs
@@ -10,35 +10,37 @@ namespace REST.Controllers
 {
     public class Animal
     {
-        [Required(ErrorMessage ="Campo obligatorio")]
         public int Id { get; set; }
+
         [Required(ErrorMessage = "Campo obligatorio")]
         public string Nombre { get; set; }
+
         [Required(ErrorMessage = "Campo obligatorio")]
         public string Especie { get; set; }
-        [Required(ErrorMessage = "Campo obligatorio")]
         public DateTime FechaCreacion { get; set; }        
         public DateTime FechaModificacion { get; set; }
-        public bool Eliminado { get; set; }
+        internal bool Eliminado { get; set; }
     }
 
 
     [RoutePrefix("api/Animal")]
     public class AnimalController : ApiController
     {
+
         private static List<Animal> ListaCompleta = new List<Animal>()
         {
-            new Animal(){ Id = 1, Nombre ="Pato", FechaCreacion = DateTime.Now, Especie="Ave" },
-            new Animal(){ Id = 2, Nombre ="Aguila", FechaCreacion = DateTime.Now, Especie="Ave" },
-            new Animal(){ Id = 3, Nombre ="Elefante", FechaCreacion = DateTime.Now, Especie="Elephantidae" },
-            new Animal(){ Id = 4, Nombre ="Perro", FechaCreacion = DateTime.Now, Especie="Canino" },
-            new Animal(){ Id = 5, Nombre ="Gato", FechaCreacion = DateTime.Now, Especie="Felino" },
+            new Animal(){ Id = 1, Nombre ="Pato", Especie="Ave", FechaCreacion = DateTime.Now },
+            new Animal(){ Id = 2, Nombre ="Aguila", Especie="Ave", FechaCreacion = DateTime.Now  },
+            new Animal(){ Id = 3, Nombre ="Elefante", Especie="Elephantidae", FechaCreacion = DateTime.Now  },
+            new Animal(){ Id = 4, Nombre ="Perro", Especie="Canino", FechaCreacion = DateTime.Now  },
+            new Animal(){ Id = 5, Nombre ="Gato", Especie="Felino", FechaCreacion = DateTime.Now  },
         };
-        // GET api/values
-        [Route("filtro")]
+
+        [Route("obtenerLista")]
         public IHttpActionResult Get(string especie = "", string nombre = "")
         {
-            List<Animal> ListaFiltrada = ListaCompleta;
+            List<Animal> ListaFiltrada = ListaCompleta.Where(x=>x.Eliminado == false).ToList();
+
             if (!string.IsNullOrEmpty(especie))
                 ListaFiltrada = ListaFiltrada.Where(x => x.Especie.Contains(especie)).ToList();
             if (!string.IsNullOrEmpty(nombre))
@@ -48,25 +50,48 @@ namespace REST.Controllers
             return Ok(ListaFiltrada);
         }
 
-        // GET api/values/5
-        public string Get(int id)
+        [Route("obtenerAnimal/{id}")]
+        public IHttpActionResult Get(int id)
         {
-            return "value";
+            if (ListaCompleta.FirstOrDefault(x => x.Id == id) != null)
+                return Ok(ListaCompleta.FirstOrDefault(x => x.Id == id));
+            return NotFound();
         }
 
-        // POST api/values
-        public void Post([FromBody] string value)
+        [Route("cargarAnimal/{especie}/{nombre}")]
+        public IHttpActionResult Post(string especie, string nombre )
         {
+            var animal = new Animal();
+            animal.Especie = especie;
+            animal.Nombre = nombre;
+            animal.Id = ListaCompleta.Last().Id + 1;
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            
+            ListaCompleta.Add(animal);
+
+            return Ok(animal);
         }
 
-        // PUT api/values/5
-        public void Put(int id, [FromBody] string value)
+        [Route("putAnimal/{id}/{nombre}/{especie}")]
+        public IHttpActionResult Put(int id, string nombre, string especie)
         {
+            var animal = ListaCompleta.FirstOrDefault(x => x.Id == id);
+            animal.Especie = especie;
+            animal.Nombre = nombre;
+            animal.FechaModificacion = DateTime.Now;
+            return Ok(animal);
         }
 
-        // DELETE api/values/5
-        public void Delete(int id)
+        [Route("deleteAnimal/{id}")]
+        public IHttpActionResult Delete(int id)
         {
+            var animal = ListaCompleta.FirstOrDefault(x => x.Id == id);
+            animal.Eliminado = true;
+
+            return Ok(animal);
         }
     }
 }
